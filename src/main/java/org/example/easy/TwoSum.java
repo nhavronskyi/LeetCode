6215f2c5994232c5984b1c99b014d7997e6a631a
nazar <nhavronskyi@gmail.com>
2023-05-03 08:21:49 +0200

    Two Sum

--- src/main/java/org/example/easy/TwoSum.java
+++ src/main/java/org/example/easy/TwoSum.java
@@ -1,7 +1,7 @@
 package org.example.easy;
 
 public class TwoSum {
-    public static int[] twoSum(int[] nums, int target) {
+    public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i+1; j < nums.length; j++) {
                 if(nums[i] + nums[j] == target){
