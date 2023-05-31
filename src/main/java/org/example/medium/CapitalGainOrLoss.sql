881f5368037998f1caf6c33be07f409b0937e986
nazar <nhavronskyi@gmail.com>
2023-05-31 07:11:11 +0200

    Sum Multiples

--- src/main/java/org/example/medium/CapitalGainOrLoss.sql
+++ src/main/java/org/example/medium/CapitalGainOrLoss.sql
@@ -1,5 +1,6 @@
 -- https://leetcode.com/problems/capital-gainloss/
-select stock_name, sum(if(operation = "Buy", -price, price))
+select stock_name,
+       sum(if(operation = "Buy", -price, price))
     as 'capital_gain_loss'
 from stocks
 group by stock_name;
