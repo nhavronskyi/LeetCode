949ec9d4526139e6bd9866204e215d8899ef6562
nazar <nhavronskyi@gmail.com>
2023-06-08 07:36:58 +0200

    formatted

--- src/test/java/easy/BinarySearchTest.java
+++ src/test/java/easy/BinarySearchTest.java
@@ -7,15 +7,16 @@ import org.junit.jupiter.api.Test;
 public class BinarySearchTest {
     private final BinarySearch binarySearch = new BinarySearch();
 
-    private final int[] test = new int[]{-1,0,3,5,9,12};
+    private final int[] test = new int[]{-1, 0, 3, 5, 9, 12};
 
     @Test
-    public void correct(){
+    public void correct() {
         Assertions.assertEquals(4, binarySearch.search(test, 9));
         Assertions.assertEquals(4, binarySearch.myBinarySearch(test, 9));
     }
+
     @Test
-    public void wrong(){
+    public void wrong() {
         Assertions.assertEquals(-1, binarySearch.myBinarySearch(test, 2));
         Assertions.assertEquals(-1, binarySearch.myBinarySearch(test, 2));
     }
