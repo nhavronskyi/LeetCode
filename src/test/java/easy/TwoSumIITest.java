4b205e7098889c2b04b364b2b48f16a5addcabb1
nazar <nhavronskyi@gmail.com>
2023-06-14 07:35:16 +0200

    TwoSumII

---
+++ src/test/java/easy/TwoSumIITest.java
@@ -0,0 +1,24 @@
+package easy;
+
+import org.example.medium.TwoSumII;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TwoSumIITest {
+    private final TwoSumII twoSumII = new TwoSumII();
+
+    @Test
+    public void positive() {
+        Assertions.assertArrayEquals(new int[]{1, 2}, twoSumII.twoSum(new int[]{2, 7, 11, 15}, 9));
+    }
+
+    @Test
+    public void negative() {
+        Assertions.assertArrayEquals(new int[]{1, 3}, twoSumII.twoSum(new int[]{-2, -3, -4}, -6));
+    }
+
+    @Test
+    public void mixed() {
+        Assertions.assertArrayEquals(new int[]{1, 2}, twoSumII.twoSum(new int[]{-1, 0}, -1));
+    }
+}
