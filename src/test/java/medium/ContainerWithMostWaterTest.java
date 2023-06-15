08afd8256ee9a2221f9157cd47349bf7630cc168
nazar <nhavronskyi@gmail.com>
2023-06-15 08:29:45 +0200

    Container With Most Water

---
+++ src/test/java/medium/ContainerWithMostWaterTest.java
@@ -0,0 +1,20 @@
+package medium;
+
+import org.example.medium.ContainerWithMostWater;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ContainerWithMostWaterTest {
+    private final ContainerWithMostWater container = new ContainerWithMostWater();
+
+    @Test
+    public void nineNumbers() {
+        Assertions.assertEquals(49, container.maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
+    }
+
+    @Test
+    public void twoNumbers() {
+        Assertions.assertEquals(1, container.maxArea(new int[]{1, 1}));
+    }
+
+}
