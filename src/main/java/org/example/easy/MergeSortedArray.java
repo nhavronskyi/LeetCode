7bf619d4ee56fc51fa857aca14bfd55f5a337397
nazar <nhavronskyi@gmail.com>
2023-06-19 08:11:30 +0200

    0 runtime

--- src/main/java/org/example/easy/MergeSortedArray.java
+++ src/main/java/org/example/easy/MergeSortedArray.java
@@ -3,17 +3,11 @@ package org.example.easy;
 
 public class MergeSortedArray {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        System.arraycopy(nums2, 0,nums1, m, n);
-
-        for (int i = 1; i < nums1.length; i++) {
-            int key = nums1[i];
-            int j = i - 1;
-
-            while (j >= 0 && nums1[j] > key) {
-                nums1[j + 1] = nums1[j];
-                j = j - 1;
-            }
-            nums1[j + 1] = key;
+        m--;
+        n--;
+        for(int i = nums1.length -1; n >= 0; i--){
+            nums1[i] = m >= 0 && nums1[m] > nums2[n] ? nums1[m--]
+                    : nums2[n--];
         }
     }
 }
