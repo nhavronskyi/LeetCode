52ddbdec8b6335b561d4f1340b6ae6d53c26d63b
nazar <nhavronskyi@gmail.com>
2023-07-27 09:16:08 +0200

    CanPlaceFlowers

---
+++ src/main/java/org/example/easy/CanPlaceFlowers.java
@@ -0,0 +1,17 @@
+package org.example.easy;
+
+public class CanPlaceFlowers {
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0) {
+                if (i - 1 < 0 || flowerbed[i - 1] == 0) {
+                    if (i + 1 >= flowerbed.length || flowerbed[i + 1] == 0) {
+                        flowerbed[i] = 1;
+                        n--;
+                    }
+                }
+            }
+        }
+        return n <= 0;
+    }
+}
---
+++ src/test/java/easy/CanPlaceFlowersTest.java
@@ -0,0 +1,21 @@
+package easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.example.easy.CanPlaceFlowers.canPlaceFlowers;
+
+public class CanPlaceFlowersTest {
+    @Test
+    public void trueTest() {
+        Assert.assertTrue(canPlaceFlowers(new int[]{1, 0, 0, 0, 1}, 1));
+        Assert.assertTrue(canPlaceFlowers(new int[]{0, 0, 1, 0, 1}, 1));
+        Assert.assertTrue(canPlaceFlowers(new int[]{0, 0, 1, 0, 0}, 1));
+    }
+
+    @Test
+    public void falseTest() {
+        Assert.assertFalse(canPlaceFlowers(new int[]{1, 0, 0, 0, 1}, 2));
+        Assert.assertFalse(canPlaceFlowers(new int[]{1, 0, 0, 0, 0, 1}, 2));
+    }
+}
