27b9d7f4f0f3aabe72d6f7dcc9b114fe286cd5b6
nazar <nhavronskyi@gmail.com>
2023-08-01 10:20:02 +0200

    Kids With the Greatest Number of Candies

---
+++ src/main/java/org/example/easy/KidsWithTheGreatestNumberOfCandies.java
@@ -0,0 +1,17 @@
+package org.example.easy;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class KidsWithTheGreatestNumberOfCandies {
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = Arrays.stream(candies)
+                .max()
+                .orElse(-1);
+
+        return Arrays.stream(candies)
+                .map(candy -> candy + extraCandies)
+                .mapToObj(candy -> candy >= max)
+                .toList();
+    }
+}
