ef4bfff6b0882dad2684b97de51a0d6c457a5de4
nazar <nhavronskyi@gmail.com>
2023-08-02 09:38:51 +0200

    Move zeroes

---
+++ src/test/java/easy/MoveZeroesTest.java
@@ -0,0 +1,24 @@
+package easy;
+
+import org.junit.Test;
+
+import static org.example.easy.MoveZeroes.moveZeroes;
+import static org.junit.Assert.assertArrayEquals;
+
+public class MoveZeroesTest {
+
+    @Test
+    public void testOne() {
+        test(new int[]{1, 3, 12, 0, 0}, new int[]{0, 1, 0, 3, 12});
+    }
+
+    @Test
+    public void testTwo() {
+        test(new int[]{0}, new int[]{0});
+    }
+
+    private void test(int[] expected, int[] actual) {
+        moveZeroes(actual);
+        assertArrayEquals(expected, actual);
+    }
+}
