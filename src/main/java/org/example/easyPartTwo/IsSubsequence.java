a3d5c944f4f2bdf4110b20170cdb0ed2c42e19b5
nazar <nhavronskyi@gmail.com>
2023-08-03 09:35:56 +0200

    IsSubsequence

---
+++ src/main/java/org/example/easyPartTwo/IsSubsequence.java
@@ -0,0 +1,13 @@
+package org.example.easyPartTwo;
+
+public class IsSubsequence {
+    public static boolean isSubsequence(String s, String t) {
+        int i = 0;
+        for (char c : t.toCharArray()) {
+            if(s.charAt(i) == c){
+                i++;
+            }
+        }
+        return i == s.length();
+    }
+}
---
+++ src/test/java/easyPartTwo/IsSubsequenceTest.java
@@ -0,0 +1,20 @@
+package easyPartTwo;
+
+import org.junit.Test;
+
+import static org.example.easyPartTwo.IsSubsequence.isSubsequence;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class IsSubsequenceTest {
+    @Test
+    public void trueTest() {
+        assertTrue(isSubsequence("abc", "ahbgdc"));
+    }
+    @Test
+    public void falseTest() {
+        assertFalse(isSubsequence("axc", "ahbgdc"));
+        assertFalse(isSubsequence("acb", "ahbgdc"));
+        assertFalse(isSubsequence("aaaaaa", "bbaaaa"));
+    }
+}
