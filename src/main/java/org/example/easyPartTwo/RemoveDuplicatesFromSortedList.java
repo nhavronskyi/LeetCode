88161bc7fef70a1f0ccb6e9e2ca1c289b77aa3fc
nazar <nhavronskyi@gmail.com>
2023-08-14 11:56:27 +0200

    RemoveDuplicatesFromSortedList

---
+++ src/main/java/org/example/easyPartTwo/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,54 @@
+package org.example.easyPartTwo;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+public class RemoveDuplicatesFromSortedList {
+    public static class ListNode {
+        public int val;
+        public ListNode next;
+
+        private ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        if (head.val == head.next.val) {
+            return deleteDuplicates(head.next);
+        } else {
+            head.next = deleteDuplicates(head.next);
+            return head;
+        }
+    }
+
+    public static ListNode convertArrToListNode(int[] arr) {
+        ListNode first = new ListNode(0);
+        ListNode curr = first;
+        for (int i : arr) {
+            curr.next = new ListNode(i);
+            curr = curr.next;
+        }
+        return first.next;
+    }
+
+    public static int[] convertListNodeToArr(ListNode head) {
+        return convertListNodeToArr(new LinkedList<>(), head).stream()
+                .mapToInt(n -> n)
+                .toArray();
+    }
+
+    private static List<Integer> convertListNodeToArr(List<Integer> list, ListNode head) {
+        return Optional.ofNullable(head)
+                .map(node -> {
+                    list.add(node.val);
+                    return convertListNodeToArr(list, node.next);
+                })
+                .orElse(list);
+    }
+}
