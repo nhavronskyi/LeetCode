41cf5c2077ea9d501d6c95cbc4771aea72987be5
nazar <nhavronskyi@gmail.com>
2023-08-22 07:04:33 +0200

    LinkedListCycle

---
+++ src/test/java/easyPartTwo/LinkedListCycleTest.java
@@ -0,0 +1,29 @@
+package easyPartTwo;
+
+import org.junit.Test;
+
+import static org.example.easyPartTwo.LinkedListCycle.convertToListNode;
+import static org.example.easyPartTwo.LinkedListCycle.hasCycle;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class LinkedListCycleTest {
+    @Test
+    public void trueTest() {
+        trueTest(3, 2, 0, -4);
+        trueTest(1, 2);
+    }
+
+    @Test
+    public void falseTest() {
+        falseTest(1);
+    }
+
+    private void trueTest(int... exp) {
+        assertTrue(hasCycle(convertToListNode(exp, true)));
+    }
+
+    private void falseTest(int... exp) {
+        assertFalse(hasCycle(convertToListNode(exp, false)));
+    }
+}
