60049f18ad95053459aa7876612db5f944118ec3
nazar <nhavronskyi@gmail.com>
2023-09-05 08:32:11 +0200

    ReverseBits

---
+++ src/test/java/year_2023/september/easy/ReverseBitsTest.java
@@ -0,0 +1,18 @@
+package year_2023.september.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.example.year_2023.september.easy.ReverseBits.*;
+
+public class ReverseBitsTest {
+    @Test
+    public void test() {
+        test(964_176_192L, "00000010100101000001111010011100");
+        test(3_221_225_471L, "11111111111111111111111111111101");
+    }
+
+    private void test(long ans, String act){
+        Assert.assertEquals(ans, convertReversedBitsToLong(reverseBits(parseBinaryToLong(act))));
+    }
+}
